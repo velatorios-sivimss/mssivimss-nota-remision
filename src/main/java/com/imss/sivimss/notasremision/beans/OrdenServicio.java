204527284f7027bfc2b27dfb456267f1048c977c
Gerardo Solís Zarate <gsolis@grupopm.mx>
2023-05-25 13:38:57 -0600

    Mas campos en detalle

--- src/main/java/com/imss/sivimss/notasremision/beans/OrdenServicio.java
+++ src/main/java/com/imss/sivimss/notasremision/beans/OrdenServicio.java
@@ -1,5 +1,6 @@
 package com.imss.sivimss.notasremision.beans;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,7 +38,7 @@ public class OrdenServicio {
 	
 	private static final String ORDENAMIENTO = " ORDER BY os.ID_ORDEN_SERVICIO DESC";
 	
-	public DatosRequest obtenerODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) {
+	public DatosRequest obtenerODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) throws UnsupportedEncodingException {
 		StringBuilder query = armaQuery(formatoFecha);
 		if (busqueda.getIdOficina() > 1) {
 			query.append(" WHERE vel.ID_DELEGACION = ").append(busqueda.getIdDelegacion());
@@ -47,13 +48,13 @@ public class OrdenServicio {
 		} 
 		query.append(ORDENAMIENTO);
         
-		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes());
+		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes("UTF-8"));
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 		
 		return request;
 	}
 	
-	public DatosRequest listadoODS(BusquedaDto busqueda) {
+	public DatosRequest listadoODS(BusquedaDto busqueda) throws UnsupportedEncodingException {
 		DatosRequest request = new DatosRequest();
 		Map<String, Object> parametro = new HashMap<>();
 		StringBuilder query = new StringBuilder("SELECT os.ID_ORDEN_SERVICIO, os.CVE_FOLIO \n");
@@ -69,13 +70,13 @@ public class OrdenServicio {
 		} 
 		query.append(ORDENAMIENTO);
 		
-		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes());
+		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes("UTF-8"));
 		parametro.put(AppConstantes.QUERY, encoded);
 		request.setDatos(parametro);
 		return request;
 	}
 	
-    public DatosRequest buscarODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) {
+    public DatosRequest buscarODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) throws UnsupportedEncodingException {
 		
     	StringBuilder query = armaQuery(formatoFecha);
     	query.append("WHERE 1 = 1");
@@ -94,21 +95,23 @@ public class OrdenServicio {
     	}
     	query.append(ORDENAMIENTO);
     	
-    	String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes());
+    	String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes("UTF-8"));
 		request.getDatos().put(AppConstantes.QUERY, encoded);
     	
 		return request;
 	}
     
-    public DatosRequest detalleODS(DatosRequest request) {
+    public DatosRequest detalleODS(DatosRequest request) throws UnsupportedEncodingException {
     	String idODS = request.getDatos().get("id").toString();
 		StringBuilder query =  new StringBuilder("SELECT os.CVE_FOLIO AS folioODS, vel.DES_VELATORIO AS nomVelatorio, \n");
 		query.append("CONCAT(IFNULL(domv.DES_CALLE,''),' ',IFNULL(domv.NUM_EXTERIOR,''),' ',IFNULL(domv.DES_COLONIA,'')) AS dirVelatorio, \n");
 		query.append("CONCAT(prf.NOM_PERSONA,' ',prf.NOM_PRIMER_APELLIDO,' ',prf.NOM_SEGUNDO_APELLIDO) AS nomFinado, \n");
-		query.append("par.DES_PARENTESCO AS parFinado, vel. NOM_RESPO_SANITARIO AS respoSanitario, \n");
+		query.append("par.DES_PARENTESCO AS parFinado, vel. NOM_RESPO_SANITARIO AS nomResponsable, \n");
 		query.append("CONCAT(prc.NOM_PERSONA,' ',prc.NOM_PRIMER_APELLIDO,' ',prc.NOM_SEGUNDO_APELLIDO) AS nomSolicitante, \n");
 		query.append("CONCAT(IFNULL(domc.DES_CALLE,''),' ',IFNULL(domc.NUM_EXTERIOR,''),' ',IFNULL(domc.DES_COLONIA,'')) AS dirSolicitante, \n");
-		query.append("prc.CVE_CURP AS curpSolicitante, vel.DES_VELATORIO AS velatorioOrigen \n");
+		query.append("prc.CVE_CURP AS curpSolicitante, vel.DES_VELATORIO AS velatorioOrigen, \n");
+		query.append("IFNULL(cvn.DES_FOLIO,0) AS folioConvenio, IFNULL(cvn.FEC_INICIO,0) AS fechaConvenio, \n");
+		query.append("(SELECT LPAD(IFNULL(MAX(NUM_FOLIO+1),1),6,'0') FROM svt_nota_remision) AS folioNota \n");
 		query.append("FROM SVC_ORDEN_SERVICIO os \n");
 		query.append("JOIN SVC_FINADO fin ON (os.ID_ORDEN_SERVICIO = fin.ID_ORDEN_SERVICIO) \n");
 		query.append("JOIN SVC_VELATORIO vel ON (vel.ID_VELATORIO = fin.ID_VELATORIO) \n");
@@ -117,26 +120,28 @@ public class OrdenServicio {
 		query.append("LEFT JOIN SVC_PARENTESCO par ON (os.ID_PARENTESCO = par.ID_PARENTESCO) \n");
 		query.append("JOIN SVC_CONTRATANTE con ON (os.ID_CONTRATANTE = con.ID_CONTRATANTE) \n");
 		query.append("JOIN SVC_PERSONA prc ON (con.ID_PERSONA = prc.ID_PERSONA) \n");
+		query.append("LEFT JOIN SVT_CONTRATANTE_PAQUETE_CONVENIO_PF cpcf ON (con.ID_CONTRATANTE = cpcf.ID_CONTRATANTE) \n");
+		query.append("LEFT JOIN SVT_CONVENIO_PF cvn ON (cpcf.ID_CONVENIO_PF = cvn.ID_CONVENIO_PF) \n");
 		query.append("LEFT JOIN SVT_DOMICILIO domc ON (con.ID_DOMICILIO = domc.ID_DOMICILIO) \n");
 		query.append("WHERE os.ID_ORDEN_SERVICIO = " + idODS);
 		
-		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes());
+		String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes("UTF-8"));
 		request.getDatos().remove("id");
 		request.getDatos().put(AppConstantes.QUERY, encoded);
 		return request;
 	}
     
-    public DatosRequest existeNotaRem(DatosRequest request) {
+    public DatosRequest existeNotaRem(DatosRequest request) throws UnsupportedEncodingException {
     	String idODS = request.getDatos().get("id").toString();
 		String query = "SELECT COUNT(NUM_FOLIO) AS valor FROM SVT_NOTA_REMISION WHERE ID_ORDEN_SERVICIO = " + idODS;
 		
-		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
+		String encoded = DatatypeConverter.printBase64Binary(query.getBytes("UTF-8"));
 		request.getDatos().remove("id");
 		request.getDatos().put(AppConstantes.QUERY, encoded);
   		return request;
   	}
     
-    public DatosRequest actualizaEstatus(String estatus) {
+    public DatosRequest actualizaEstatus(String estatus) throws UnsupportedEncodingException {
     	DatosRequest request = new DatosRequest();
 		Map<String, Object> parametro = new HashMap<>();
 		final QueryHelper q = new QueryHelper("UPDATE SVC_ORDEN_SERVICIO");
@@ -144,7 +149,7 @@ public class OrdenServicio {
 		q.addWhere("ID_ORDEN_SERVICIO = " + this.id);
 		
 		String query = q.obtenerQueryActualizar();
-		String encoded = DatatypeConverter.printBase64Binary(query.getBytes());
+		String encoded = DatatypeConverter.printBase64Binary(query.getBytes("UTF-8"));
 		parametro.put(AppConstantes.QUERY, encoded);
 		request.setDatos(parametro);
 		return request;
@@ -155,7 +160,7 @@ public class OrdenServicio {
 		query.append("IFNULL(cvn.DES_FOLIO,0) AS folioConvenio, os.ID_CONTRATANTE AS idContratante, \n");
 		query.append("CONCAT(prc.NOM_PERSONA,' ',prc.NOM_PRIMER_APELLIDO,' ',prc.NOM_SEGUNDO_APELLIDO) AS nomContratante, \n");
 		query.append("fin.ID_FINADO AS idFinado, CONCAT(prf.NOM_PERSONA,' ',prf.NOM_PRIMER_APELLIDO,' ',prf.NOM_SEGUNDO_APELLIDO) AS nomFinado, \n");
-		query.append("IFNULL(nr.ID_ESTATUS,1) AS estatus, IFNULL(nr.ID_NOTAREMISION,0) AS idNota, nrc.ID_NOTAREMISION AS idCancelada \n");
+		query.append("IFNULL(nr.ID_ESTATUS,1) AS estatus, IFNULL(nr.ID_NOTAREMISION,0) AS idNota, IFNULL(nr.ID_NOTAREMISION,0) AS idCancelada \n");
 		query.append("FROM SVC_ORDEN_SERVICIO os \n");
 		query.append("LEFT JOIN SVC_INFORMACION_SERVICIO inf ON (os.ID_ORDEN_SERVICIO = inf.ID_ORDEN_SERVICIO) \n");
 		query.append("JOIN SVC_CONTRATANTE con ON (os.ID_CONTRATANTE = con.ID_CONTRATANTE) \n");
@@ -164,8 +169,7 @@ public class OrdenServicio {
 		query.append("LEFT JOIN SVT_CONVENIO_PF cvn ON (cpcf.ID_CONVENIO_PF = cvn.ID_CONVENIO_PF) \n");
 		query.append("JOIN SVC_FINADO fin ON (os.ID_ORDEN_SERVICIO = fin.ID_ORDEN_SERVICIO) \n");
 		query.append("JOIN SVC_PERSONA prf ON (fin.ID_PERSONA = prf.ID_PERSONA) \n");
-		query.append("LEFT JOIN SVT_NOTA_REMISION nr ON (os.ID_ORDEN_SERVICIO = nr.ID_ORDEN_SERVICIO) \n");
-		query.append("LEFT JOIN SVT_NOTA_REMISION nrc ON (os.ID_ORDEN_SERVICIO = nrc.ID_ORDEN_SERVICIO AND nrc.ID_ESTATUS = 3) ");
+		query.append("LEFT JOIN SVT_NOTA_REMISION nr ON (os.ID_ORDEN_SERVICIO = nr.ID_ORDEN_SERVICIO AND nr.ID_ESTATUS = 3) \n");
 		query.append("LEFT JOIN SVC_VELATORIO vel ON (fin.ID_VELATORIO = vel.ID_VELATORIO) \n");
 		
 		return query;
