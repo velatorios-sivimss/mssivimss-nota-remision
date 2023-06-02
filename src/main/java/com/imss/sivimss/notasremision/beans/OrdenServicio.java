60e971deb6655372c2f53b21b97f1c799c7f891f
mikanyel <mikanyel@gmail.com>
2023-06-02 12:48:32 -0600

    Se cambia el query para mostrar el resultado de la busqueda cone le ajuste en las ODS canceladas

--- src/main/java/com/imss/sivimss/notasremision/beans/OrdenServicio.java
+++ src/main/java/com/imss/sivimss/notasremision/beans/OrdenServicio.java
@@ -3,9 +3,11 @@ package com.imss.sivimss.notasremision.beans;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import com.imss.sivimss.notasremision.util.AppConstantes;
@@ -19,9 +21,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Setter
 @Getter
 public class OrdenServicio {
@@ -35,6 +34,7 @@ public class OrdenServicio {
 	private Integer idFinado;
 	private String nomFinado;
 	private Integer estatus;
+	private static final Logger logger = (Logger) LoggerFactory.getLogger(OrdenServicio.class);
 	
 	public DatosRequest obtenerODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) throws UnsupportedEncodingException {
 		StringBuilder query = armaQuery(formatoFecha);
@@ -73,33 +73,80 @@ public class OrdenServicio {
 	}
 	
     public DatosRequest buscarODS(DatosRequest request, BusquedaDto busqueda, String formatoFecha) throws UnsupportedEncodingException {
-		
-    	StringBuilder query = armaQuery(formatoFecha);
-    	boolean otroFiltro = false;
-    	if (busqueda.getIdVelatorio() != null) {
-    		otroFiltro = true;
-			query.append(" AND os.ID_VELATORIO = ").append(busqueda.getIdVelatorio());
-		}
-    	if (busqueda.getIdDelegacion() != null) {
-    		otroFiltro = true;
+    	StringBuilder query = new StringBuilder("");
+    	StringBuilder busquedaCancelada = busquedaCancelada(formatoFecha);
+    	StringBuilder busquedaGeneradas = busquedaGeneradas(formatoFecha);
+    	StringBuilder busquedaSinNota = busquedaSinNota(formatoFecha);
+    	
+    	
+    	//Busqueda por dos fechas, regresa las notas con estatus Generada
+    	if (busqueda.getIdDelegacion() != null && busqueda.getIdVelatorio() != null && busqueda.getFecIniODS() != null && busqueda.getFecFinODS() != null) {
     		query.append(" AND vel.ID_DELEGACION = ").append(busqueda.getIdDelegacion());
-    	}
+    		query.append(" AND os.ID_VELATORIO = ").append(busqueda.getIdVelatorio());
+    		query.append(" AND nr.FEC_ALTA BETWEEN STR_TO_DATE('" + busqueda.getFecIniODS() + "','" + formatoFecha + "') AND STR_TO_DATE('" + busqueda.getFecFinODS() + "','" + formatoFecha + "')");
+    		busquedaGeneradas.append(query);
+    		
+    		logger.info(busquedaGeneradas.toString());
+        	String encoded = DatatypeConverter.printBase64Binary(busquedaGeneradas.toString().getBytes("UTF-8"));
+    		request.getDatos().put(AppConstantes.QUERY, encoded);
+        	
+    		return request;
+		}
+    	else if (busqueda.getIdDelegacion() != null && busqueda.getIdVelatorio() == null && busqueda.getFecIniODS() != null && busqueda.getFecFinODS() != null) {
+    		query.append(" AND vel.ID_DELEGACION = ").append(busqueda.getIdDelegacion());
+    		query.append(" AND nr.FEC_ALTA BETWEEN STR_TO_DATE('" + busqueda.getFecIniODS() + "','" + formatoFecha + "') AND STR_TO_DATE('" + busqueda.getFecFinODS() + "','" + formatoFecha + "')");
+    		busquedaGeneradas.append(query);
+    		
+    		logger.info(busquedaGeneradas.toString());
+    		String encoded = DatatypeConverter.printBase64Binary(busquedaGeneradas.toString().getBytes("UTF-8"));
+    		request.getDatos().put(AppConstantes.QUERY, encoded);
+        	
+    		return request;
+      	}
+    	else if (busqueda.getIdDelegacion() == null && busqueda.getIdVelatorio() == null && busqueda.getFecIniODS() != null && busqueda.getFecFinODS() != null) {
+    		query.append(" AND nr.FEC_ALTA BETWEEN STR_TO_DATE('" + busqueda.getFecIniODS() + "','" + formatoFecha + "') AND STR_TO_DATE('" + busqueda.getFecFinODS() + "','" + formatoFecha + "')");
+    		busquedaGeneradas.append(query);
+    		
+    		logger.info(busquedaGeneradas.toString());
+    		String encoded = DatatypeConverter.printBase64Binary(busquedaGeneradas.toString().getBytes("UTF-8"));
+    		request.getDatos().put(AppConstantes.QUERY, encoded);
+        	
+    		return request;
+		}
+    	// Busqueda General
+    	else {
+    		if (busqueda.getIdVelatorio() != null) {
+    			query.append(" AND os.ID_VELATORIO = ").append(busqueda.getIdVelatorio());
+    		}
+        	if (busqueda.getIdDelegacion() != null) {
+        		query.append(" AND vel.ID_DELEGACION = ").append(busqueda.getIdDelegacion());
+        	}
 
-    	if (busqueda.getFolioODS() != null) {
-    		otroFiltro = true;
-    	    query.append(" AND os.CVE_FOLIO = '" + busqueda.getFolioODS() +"' ");
-    	}
-    	if (busqueda.getFecIniODS() != null) {
-    	    query.append(" AND nr.FEC_ALTA BETWEEN STR_TO_DATE('" + busqueda.getFecIniODS() + "','" + formatoFecha + "') AND STR_TO_DATE('" + busqueda.getFecFinODS() + "','" + formatoFecha + "')");
-    	    if (!otroFiltro) {
-    	    	query.append(" AND nr.ID_ESTATUS = 2 ORDER BY os.FEC_ALTA");
-    	    }
+        	if (busqueda.getFolioODS() != null) {
+        	    query.append(" AND os.CVE_FOLIO = '" + busqueda.getFolioODS() +"' ");
+        	}
+        	if (busqueda.getFecIniODS() != null) {
+        	    query.append(" AND nr.FEC_ALTA BETWEEN STR_TO_DATE('" + busqueda.getFecIniODS() + "','" + formatoFecha + "') AND STR_TO_DATE('" + busqueda.getFecFinODS() + "','" + formatoFecha + "')");
+        	}
+        	
+        	 busquedaCancelada.append(query);
+        	 busquedaSinNota.append(query);
+        	 
+         	StringBuilder queryCompleto = new StringBuilder("");
+        	queryCompleto.append("SELECT  * FROM ( ");
+        	queryCompleto.append(busquedaCancelada);
+        	queryCompleto.append(" UNION ALL ");
+        	queryCompleto.append(busquedaSinNota);
+        	queryCompleto.append(" ) datos  ORDER BY datos.id");
+        	
+        	logger.info(queryCompleto.toString());
+        	String encoded = DatatypeConverter.printBase64Binary(queryCompleto.toString().getBytes("UTF-8"));
+    		request.getDatos().put(AppConstantes.QUERY, encoded);
+        	
+    		return request;
     	}
     	
-    	String encoded = DatatypeConverter.printBase64Binary(query.toString().getBytes("UTF-8"));
-		request.getDatos().put(AppConstantes.QUERY, encoded);
-    	
-		return request;
+
 	}
     
     public DatosRequest detalleODS(DatosRequest request) throws UnsupportedEncodingException {
@@ -154,6 +201,68 @@ public class OrdenServicio {
 		parametro.put(AppConstantes.QUERY, encoded);
 		request.setDatos(parametro);
 		return request;
+    }
+    
+    private StringBuilder busquedaCancelada(String formatoFecha) {
+    	StringBuilder query = new StringBuilder("SELECT os.ID_ORDEN_SERVICIO AS id, os.CVE_FOLIO AS folioODS, DATE_FORMAT(os.FEC_ALTA,'" + formatoFecha + "') AS fechaODS, IFNULL(cvn.DES_FOLIO,0) AS folioConvenio, os.ID_CONTRATANTE AS idContratante, CONCAT(prc.NOM_PERSONA,' ',prc.NOM_PRIMER_APELLIDO,' ',prc.NOM_SEGUNDO_APELLIDO) AS nomContratante, \n");
+    	query.append("fin.ID_FINADO AS idFinado,  \n");
+    	query.append("CONCAT(prf.NOM_PERSONA,' ',prf.NOM_PRIMER_APELLIDO,' ',prf.NOM_SEGUNDO_APELLIDO) AS nomFinado,  \n");
+    	query.append("IFNULL(nr.ID_ESTATUS,1) AS estatus, IFNULL(nr.ID_NOTAREMISION,0) AS idNota,  \n");
+    	query.append("IFNULL(nr.ID_NOTAREMISION,0) AS idCancelada,0 AS total \n");
+    	query.append("FROM SVC_ORDEN_SERVICIO os \n");
+    	query.append("JOIN SVC_CONTRATANTE con ON (os.ID_CONTRATANTE = con.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVC_PERSONA prc ON (con.ID_PERSONA = prc.ID_PERSONA) \n");
+    	query.append("JOIN SVT_CONTRATANTE_PAQUETE_CONVENIO_PF cpcf ON (con.ID_CONTRATANTE = cpcf.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVT_CONVENIO_PF cvn ON (cpcf.ID_CONVENIO_PF = cvn.ID_CONVENIO_PF) \n");
+    	query.append("JOIN SVC_FINADO fin ON (os.ID_ORDEN_SERVICIO = fin.ID_ORDEN_SERVICIO) \n");
+    	query.append("JOIN SVC_PERSONA prf ON (fin.ID_PERSONA = prf.ID_PERSONA) \n");
+    	query.append("JOIN SVT_NOTA_REMISION nr ON (os.ID_ORDEN_SERVICIO = nr.ID_ORDEN_SERVICIO) AND nr.ID_ESTATUS =3 \n");
+    	query.append("WHERE os.ID_ESTATUS_ORDEN_SERVICIO > 1 \n");
+    	return query;
+    }
+    
+    private StringBuilder busquedaGeneradas(String formatoFecha) {
+    	StringBuilder query = new StringBuilder("SELECT os.ID_ORDEN_SERVICIO AS id, os.CVE_FOLIO AS folioODS, \n");
+    	query.append("DATE_FORMAT(os.FEC_ALTA,'" + formatoFecha + "') AS fechaODS,  \n");
+    	query.append("IFNULL(cvn.DES_FOLIO,0) AS folioConvenio, os.ID_CONTRATANTE AS idContratante, \n");
+    	query.append("CONCAT(prc.NOM_PERSONA,' ',prc.NOM_PRIMER_APELLIDO,' ',prc.NOM_SEGUNDO_APELLIDO) AS nomContratante,  \n");
+    	query.append("fin.ID_FINADO AS idFinado, CONCAT(prf.NOM_PERSONA,' ',prf.NOM_PRIMER_APELLIDO,' ',prf.NOM_SEGUNDO_APELLIDO) AS nomFinado, \n");
+    	query.append("IFNULL(nr.ID_ESTATUS,1) AS estatus, IFNULL(nr.ID_NOTAREMISION,0) AS idNota,  \n");
+    	query.append("IFNULL(nr.ID_NOTAREMISION,0) AS idCancelada, (SELECT COUNT(rn.ID_ORDEN_SERVICIO) FROM SVT_NOTA_REMISION rn WHERE rn.ID_ORDEN_SERVICIO=os.ID_ORDEN_SERVICIO )  \n");
+    	query.append("FROM SVC_ORDEN_SERVICIO os \n");
+    	query.append("JOIN SVC_CONTRATANTE con ON (os.ID_CONTRATANTE = con.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVC_PERSONA prc ON (con.ID_PERSONA = prc.ID_PERSONA) \n");
+    	query.append("JOIN SVT_CONTRATANTE_PAQUETE_CONVENIO_PF cpcf ON (con.ID_CONTRATANTE = cpcf.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVT_CONVENIO_PF cvn ON (cpcf.ID_CONVENIO_PF = cvn.ID_CONVENIO_PF) \n");
+    	query.append("JOIN SVC_FINADO fin ON (os.ID_ORDEN_SERVICIO = fin.ID_ORDEN_SERVICIO) \n");
+    	query.append("JOIN SVC_PERSONA prf ON (fin.ID_PERSONA = prf.ID_PERSONA) \n");
+    	query.append("JOIN SVT_NOTA_REMISION nr ON (os.ID_ORDEN_SERVICIO = nr.ID_ORDEN_SERVICIO) \n");
+    	query.append("AND nr.ID_ESTATUS =2 \n");
+    	query.append("WHERE os.ID_ESTATUS_ORDEN_SERVICIO > 1 \n");
+
+    	return query;
+    }
+    
+    private StringBuilder busquedaSinNota(String formatoFecha) {
+    	StringBuilder query = new StringBuilder("SELECT os.ID_ORDEN_SERVICIO AS id, os.CVE_FOLIO AS folioODS, \n");
+    	query.append("DATE_FORMAT(os.FEC_ALTA,'" + formatoFecha + "') AS fechaODS,  \n");
+    	query.append("IFNULL(cvn.DES_FOLIO,0) AS folioConvenio, os.ID_CONTRATANTE AS idContratante,  \n");
+    	query.append("CONCAT(prc.NOM_PERSONA,' ',prc.NOM_PRIMER_APELLIDO,' ',prc.NOM_SEGUNDO_APELLIDO) AS nomContratante, \n");
+    	query.append("fin.ID_FINADO AS idFinado, CONCAT(prf.NOM_PERSONA,' ',prf.NOM_PRIMER_APELLIDO,' ',prf.NOM_SEGUNDO_APELLIDO) AS nomFinado, \n");
+    	query.append("IFNULL(nr.ID_ESTATUS,1) AS estatus, IFNULL(nr.ID_NOTAREMISION,0) AS idNota,  \n");
+    	query.append("IFNULL(nr.ID_NOTAREMISION,0) AS idCancelada, (SELECT COUNT(rn.ID_ORDEN_SERVICIO) FROM SVT_NOTA_REMISION rn WHERE rn.ID_ORDEN_SERVICIO=os.ID_ORDEN_SERVICIO ) \n");
+    	query.append("FROM SVC_ORDEN_SERVICIO os \n");
+    	query.append("JOIN SVC_CONTRATANTE con ON (os.ID_CONTRATANTE = con.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVC_PERSONA prc ON (con.ID_PERSONA = prc.ID_PERSONA) \n");
+    	query.append("JOIN SVT_CONTRATANTE_PAQUETE_CONVENIO_PF cpcf ON (con.ID_CONTRATANTE = cpcf.ID_CONTRATANTE) \n");
+    	query.append("JOIN SVT_CONVENIO_PF cvn ON (cpcf.ID_CONVENIO_PF = cvn.ID_CONVENIO_PF) \n");
+    	query.append("JOIN SVC_FINADO fin ON (os.ID_ORDEN_SERVICIO = fin.ID_ORDEN_SERVICIO) \n");
+    	query.append("JOIN SVC_PERSONA prf ON (fin.ID_PERSONA = prf.ID_PERSONA) \n");
+    	query.append("LEFT JOIN SVT_NOTA_REMISION nr ON (os.ID_ORDEN_SERVICIO = nr.ID_ORDEN_SERVICIO) \n");
+    	query.append("AND nr.ID_ESTATUS =2 \n");
+    	query.append("WHERE os.ID_ESTATUS_ORDEN_SERVICIO > 1 \n");
+    	
+    	return query;
     }
     
     private StringBuilder armaQuery(String formatoFecha) {
